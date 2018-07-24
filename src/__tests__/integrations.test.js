04b36aef1ea6ca7dedd5c3474de674d8087f1748
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-24 16:57:02 +0200

    introduce a pause

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -16,7 +16,7 @@ afterEach(() => {
   moxios.uninstall();
 });
 
-it('should fetch a list of comments and display them', () => {
+it('should fetch a list of comments and display them', done => {
   const wrapped = mount(
     <Root>
       <App />
@@ -25,5 +25,9 @@ it('should fetch a list of comments and display them', () => {
 
   wrapped.find('.fetch-comments').simulate('click');
 
-  expect(wrapped.find('li').length).toEqual(2);
+  setTimeout(() => {
+    wrapped.update();
+    expect(wrapped.find('li').length).toEqual(2);
+    done();
+  }, 100);
 });
